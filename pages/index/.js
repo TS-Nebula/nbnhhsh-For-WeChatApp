e3d4b531d4c35d5dcdb0e7ea30caa6f460a30d3c
FutureApple <fa-dream@foxmail.com>
2020-08-20 21:30:33 +0800

    init app
    init the wechat app

--- pages/index/.js
+++ pages/index/.js
@@ -1,4 +1,4 @@
-// pages/logs/logs.js
+// pages/index/.js
 Page({
 
   /**
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,52 @@
 //index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+
+  onLoad: function(options) {
+    // 页面创建时执行
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onShow: function() {
+    // 页面出现在前台时执行
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  onReady: function() {
+    // 页面首次渲染完毕时执行
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  onHide: function() {
+    // 页面从前台变为后台时执行
+  },
+  onUnload: function() {
+    // 页面销毁时执行
+  },
+  onPullDownRefresh: function() {
+    // 触发下拉刷新时执行
+  },
+  onReachBottom: function() {
+    // 页面触底时执行
+  },
+  onShareAppMessage: function () {
+    // 页面被用户分享时执行
+  },
+  onPageScroll: function() {
+    // 页面滚动时执行
+  },
+  onResize: function() {
+    // 页面尺寸变化时执行
+  },
+  onTabItemTap(item) {
+    // tab 点击时执行
+    console.log(item.index)
+    console.log(item.pagePath)
+    console.log(item.text)
+  },
+  // 事件响应函数
+  viewTap: function() {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      text: 'Set some data for updating view.'
+    }, function() {
+      // this is setData callback
     })
+  },
+  // 自由数据
+  customData: {
+    hi: 'MINA'
   }
 })
