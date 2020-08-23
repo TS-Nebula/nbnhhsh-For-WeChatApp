ec5c27a07faa6a07af1fff6e4011003112a7e9aa
FutureApple <fa-dream@foxmail.com>
2020-08-23 21:46:56 +0800

    完工，后续优化Ui

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,28 +1,40 @@
 Page({
   data: {
     result:"暂无数据",
+
  },
 
   watchPassWord: function (event) {
-    var a =(event.detail.value);
+    var requests =(event.detail.value);
+    this.setData({
+      sercsData: requests
+    })
  },
 
   btnclick : function(event){
+    let a = this.data.sercsData
+    let _this = this
     wx.request({
-      url: 'https://lab.magiconch.com/api/nbnhhsh/guess', 
-      data: {
-        x: '',
-        y: ''
-      },
+      url: 'https://lab.magiconch.com/api/nbnhhsh/guess',
+      method: 'POST',
       header: {
-        'content-type': 'application/json' // 默认值
+        'content-type': 'application/x-www-form-urlencoded'
       },
-      success (res) {
-        console.log(res.data)
+      data: {
+        text: a
+      },
+      success(res) {
+        console.log(res.data[0].trans[0])
+        let trans = res.data[0].trans
+        let words = ''
+        for (let i in trans){
+          console.log(i)
+          words += trans[i]+ " "
+        }
+        _this.setData({
+          result: words
+        })
       }
-    })
-    this.setData({
-      result: a
-    })
+  })
   }
 })
