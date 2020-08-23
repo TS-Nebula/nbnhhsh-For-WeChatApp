bcf888d295e91a7c054e8039c4678d861f3eafbb
FutureApple <fa-dream@foxmail.com>
2020-08-23 20:53:21 +0800

    Ui

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,28 @@
 Page({
   data: {
-    result:"暂无数据"
+    result:"暂无数据",
+ },
+
+  watchPassWord: function (event) {
+    var a =(event.detail.value);
+ },
+
+  btnclick : function(event){
+    wx.request({
+      url: 'https://lab.magiconch.com/api/nbnhhsh/guess', 
+      data: {
+        x: '',
+        y: ''
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log(res.data)
+      }
+    })
+    this.setData({
+      result: a
+    })
   }
 })
