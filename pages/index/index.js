c6880e32bcf5a4098ea412d4f5060361f3b184a5
FutureApple <fa-dream@foxmail.com>
2020-08-23 17:33:01 +0800

    Ui

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,52 +1,5 @@
-//index.js
 Page({
-
-  onLoad: function(options) {
-    // 页面创建时执行
-  },
-  onShow: function() {
-    // 页面出现在前台时执行
-  },
-  onReady: function() {
-    // 页面首次渲染完毕时执行
-  },
-  onHide: function() {
-    // 页面从前台变为后台时执行
-  },
-  onUnload: function() {
-    // 页面销毁时执行
-  },
-  onPullDownRefresh: function() {
-    // 触发下拉刷新时执行
-  },
-  onReachBottom: function() {
-    // 页面触底时执行
-  },
-  onShareAppMessage: function () {
-    // 页面被用户分享时执行
-  },
-  onPageScroll: function() {
-    // 页面滚动时执行
-  },
-  onResize: function() {
-    // 页面尺寸变化时执行
-  },
-  onTabItemTap(item) {
-    // tab 点击时执行
-    console.log(item.index)
-    console.log(item.pagePath)
-    console.log(item.text)
-  },
-  // 事件响应函数
-  viewTap: function() {
-    this.setData({
-      text: 'Set some data for updating view.'
-    }, function() {
-      // this is setData callback
-    })
-  },
-  // 自由数据
-  customData: {
-    hi: 'MINA'
+  data: {
+    result:"暂无数据"
   }
 })
